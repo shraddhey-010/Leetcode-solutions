71414e345086ebde318f13b6b961f769d8d1aa09
shraddhey-010 <shraddhey1010101@gmail.com>
2026-05-04 23:49:50 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (98.27%) - LeetSync

---
+++ 1642-water-bottles/water-bottles.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int numWaterBottles(int numBottles, int numExchange) 
+    {
+     int ans=  numBottles;
+     while( numBottles>=numExchange)  
+     {
+        int  newBottles= numBottles / numExchange;
+        int  remBottles=numBottles % numExchange;
+        ans=ans+newBottles;
+        numBottles=remBottles + newBottles;
+
+     }
+     return ans;
+
+    }
+};
